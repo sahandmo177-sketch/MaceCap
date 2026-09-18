07f2e95e0299985cfb2dec69d3a97db0337c3f1d
sahandmo177-sketch <sahandmo177@gmail.com>
2026-09-18 13:25:25 +0100

    Update MaceCap.java

--- src/main/java/de/steak/macecap/MaceCap.java
+++ src/main/java/de/steak/macecap/MaceCap.java
@@ -1,1 +1,27 @@
+package de.steak.macecap;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MaceCap extends JavaPlugin implements Listener {
+
+    private static final double MAX_DAMAGE = 6.5;
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+        getLogger().info("MaceCap aktiviert - Maximum: 6.5 Schaden");
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onDamage(EntityDamageEvent event) {
+        String type = event.getDamageSource().getDamageType().key().value();
+
+        if (type.equals("mace_smash") && event.getDamage() > MAX_DAMAGE) {
+            event.setDamage(MAX_DAMAGE);
+        }
+    }
+}
