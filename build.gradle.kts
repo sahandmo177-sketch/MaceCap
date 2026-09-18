61c4f39022c119dfae31917eb9e69287584afc61
sahandmo177-sketch <sahandmo177@gmail.com>
2026-09-18 13:20:08 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,24 @@
+plugins {
+    java
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.19"
+}
 
+group = "de.steak"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    paperweight.paperDevBundle("26.2.build.+")
+}
+
+java {
+    toolchain.languageVersion = JavaLanguageVersion.of(25)
+}
+
+tasks.jar {
+    archiveBaseName.set("MaceCap")
+}
